73f9b0976eebfcb814df1ddb47ad30f32c7f9273
Gusain, Nidhi <NidhiGusain@quickenloans.com>
2021-09-14 22:26:00 -0400

    Checking in lab

---
+++ CoffeeShop/CoffeeShopProduct/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using CoffeeShopProduct.Models;
+
+namespace CoffeeShopProduct.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            MySqlConnection db = new MySqlConnection("Server=localhost;Database=coffeeshop;Uid=root;Password=JaiHanuman@84");
+            List<Product> products = db.GetAll<Product>().ToList();
+            return View(products);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            MySqlConnection db = new MySqlConnection("Server=localhost;Database=coffeeshop;Uid=root;Password=JaiHanuman@84");
+            List<Product> prod = db.Query<Product>("select * from products where id=@myId", new { myId = id }).ToList();
+            return View(prod);
+        }
+
+    }
+}
---
+++ CoffeeShop/CoffeeShopProduct/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
+
+
+namespace CoffeeShopProduct.Models
+{
+    [Table("products")]
+    public class Product
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public float price { get; set; }
+        public string category { get; set; }
+
+    }
+}
